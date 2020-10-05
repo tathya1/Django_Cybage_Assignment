54e0a29f0a9790197802018d4cca1839e1f02b50
tathya1 <tathya4225@gmail.com>
2020-10-05 17:10:05 +0530

    Modeling done

--- employee_app/models.py
+++ employee_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Department(models.Model):
+    departmentName = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.departmentName
+
+
+class Designation(models.Model):
+    designationName = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.designationName
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    designation = models.ForeignKey(Designation, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
