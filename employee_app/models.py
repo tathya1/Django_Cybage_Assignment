f4ee6eaad5757f573327fb8b90c6c536bc171855
tathya1 <tathya4225@gmail.com>
2020-10-06 14:56:43 +0530

    ORM updated

--- employee_app/models.py
+++ employee_app/models.py
@@ -15,10 +15,9 @@ class Designation(models.Model):
 
 class Employee(models.Model):
     name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    email = models.EmailField(unique=True, blank=False)
+    department = models.ManyToManyField(Department)
     designation = models.ForeignKey(Designation, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
-
